e24e687774ddca07297aceeae87fa79e26c331ab
Aiman <aimnhkmmn05@gmail.com>
2025-11-16 02:06:56 +0800

    init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.assign1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens com.example.assign1 to javafx.fxml;
+    exports com.example.assign1;
+}
